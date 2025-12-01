b751fbcb7e4c84e5bc80a622198f81956dbe1733
Mark Larah <mark@larah.me>
2025-11-30 22:02:27 -0600

    init

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # mush
+
+## Your local makefile
+
+Include this in your makefile:
+
+```mk
+ECR_REGISTRY = ""  # e.g. 123456789.dkr.ecr.us-east-1.amazonaws.com
+REPOSITORY   = ""  # e.g. library/ubuntu
+PLATFORM     = ""  # e.g. linux/amd64
+HOST         = ""  # e.g. root@1.2.3.4
+
+include ~/apps/mush/mush.mk
+```
+
+...then run `$ make deploy-<version>`
---
+++ mush.mk
@@ -0,0 +1,10 @@
+deploy-%:
+	@echo "Building and deploying version $*..."
+	podman build --platform $(PLATFORM) -t $(ECR_REGISTRY)/$(IMAGE_NAME):$* .
+	podman tag $(ECR_REGISTRY)/$(IMAGE_NAME):$* $(ECR_REGISTRY)/$(IMAGE_NAME):latest
+	podman push $(ECR_REGISTRY)/$(IMAGE_NAME):$*
+	podman push $(ECR_REGISTRY)/$(IMAGE_NAME):latest
+	@echo "Successfully deployed version $* and latest to ECR"
+	ssh $(HOST) "docker pull $(ECR_REGISTRY)/$(IMAGE_NAME):latest"
+	ssh $(HOST) "docker compose --project-directory /root/docker up -d --remove-orphans"
+	@echo "Successfully deployed new version $* on host"
